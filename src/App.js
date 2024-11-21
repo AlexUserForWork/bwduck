98e70547779b784cc86473245f70d208fee18672
Kononiuk <hmasteraaa@gmail.com>
2024-11-21 10:10:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+   return (
+      <div className='App'>
+         {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+      </div>
+   );
 }
 
 export default App;
